b86cf9f69f0e77309e0d9ce22cf62e71f2039c76
gisma_gismo <gisma4x4@mail.ru>
2022-04-16 20:32:59 +0300

    upd

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,13 @@
 import { cons } from '@hexlet/pairs';
 import { getRandom, isEven } from '../functions.js';
 
-const generateRoundData = () => {
+export default () => {
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
   const roundData = [];
   for (let i = 0; i < 3; i += 1) {
-    const number = String(getRandom(0, 100));
+    const number = String(getRandom(0, 500));
     const answer = isEven(number) ? 'yes' : 'no';
     roundData.push(cons(number, answer));
   }
   return [roundData, rule];
 };
-export default generateRoundData;
